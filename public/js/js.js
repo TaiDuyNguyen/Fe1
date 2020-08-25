2438f9c4b63ce31da865b3c707c23b25f5cc6199
TaiDuyNguyen <68818518+TaiDuyNguyen@users.noreply.github.com>
2020-08-25 11:04:37 +0700

    Fe1

---
+++ public/js/js.js
@@ -0,0 +1,7 @@
+var headerH = $(window).height();
+var navHeader = $('#nav-header-top ').height();
+  $('#header').css('height', headerH - navHeader);
+  var navHeader2 = $('.nav-top-view ').height();
+  $('.slide_left').css('height', headerH -navHeader2);
+
+   
